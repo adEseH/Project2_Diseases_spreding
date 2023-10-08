d5dc1cf864fb358ee762da346f876e10610425e2
adEseH <annarockstroh@outlook.de>
2023-10-08 16:48:46 +0200

    Updated travaling matrix

--- Second Project/Diseases.py
+++ Second Project/Diseases.py
@@ -10,53 +10,112 @@ import matplotlib.pyplot as plt
 #### City 1: Mexico City, City 2: Vancouver, City 3: New York
 
 class Disease:
-    def __init__(self,T=300, a=1/6, b = [0.287, 0.286,0.188],R0 = [2.5, 1.6, 1.74], g = [0.00253,0.0029, 0.0018],
-                         pop_0=np.array([[19.985E6,4,0],[2.238E6,1000,0], [18.309E6,1000,0]]), h=1 ):
+    def __init__(self, T=300, a=1/6, R0 = [1.72, 1.6, 1.7], g = [0.00253,0.0029, 0.0018],
+                         pop_0=np.array([[19.985E6,10,0],[2.238E6,0,0], [18.309E6,0,0]]), h=0.2 ):
         
         # Time and population 
+        self.R0 = R0
+        self.a = a
+        self.g = g
+
         self.t0 = 0
         self.T = T # total time [default: 200 days]
         self.population_0 = pop_0 # inital population [S0, I0, R0] (y0)
         self.N = round(sum(pop_0[0]) +sum(pop_0[1]) +sum(pop_0[2]), 0) # Overall population
 
-        # initialise gamma_arrays: (standart: constant values over time)
+
+        #### different choices of beta and gamma
+
+        # with vaccination, constant beta, constant gamma
         if False:
             self.alpha = a
             # Calculation beta
             b = [a*R0[0], a*R0[1], a*R0[2]]
-            self.beta_array= np.array([np.repeat(b[0], T/h),np.repeat(b[1], T/h),np.repeat(b[2], T/h)])
-            self.gamma_array = g= np.array([np.repeat(g[0], T/h),np.repeat(g[1], T/h),np.repeat(g[2], T/h)])
+            self.beta_array = np.array([np.repeat(b[0], T/h),np.repeat(b[1], T/h),np.repeat(b[2], T/h)])
+            self.gamma_array =  np.array([np.repeat(g[0], T/h),np.repeat(g[1], T/h),np.repeat(g[2], T/h)])
 
-        # No vaccination/ quarantine/ travaling
+        elif True: # with vaccination, constant beta, gauss gamma
+            self.T = T
+            self.alpha = 1/6
+            T_vec = np.arange(0,T,h)
+            gamma_mexico = 180*self.g[0] / (np.sqrt(2.0 * np.pi) * 30) * np.exp(-np.power((T_vec - 90) / 30, 2.0) / 2)
+            gamma_van = 180*self.g[1] / (np.sqrt(2.0 * np.pi) * 30) * np.exp(-np.power((T_vec - 90) / 30, 2.0) / 2)
+            gamma_NY = 180*self.g[2] / (np.sqrt(2.0 * np.pi) * 30) * np.exp(-np.power((T_vec - 90) / 30, 2.0) / 2)
+            # Calculation beta
+            b = [a*R0[0], a*R0[1], a*R0[2]]
+            self.beta_array= np.array([np.repeat(b[0], T/h),np.repeat(b[1], T/h),np.repeat(b[2], T/h)])
+
+            self.gamma_array =  np.array([gamma_mexico,gamma_van,gamma_NY])
+
+        elif False: # with vaccination, linear beta, gauss gamma
+            self.T = T
+            self.alpha = 1/6
+            T_vec = np.arange(0,T,h)
+            #beta
+            b = [a*R0[0], a*R0[1], a*R0[2]]
+            b1 = [np.repeat(b[0], 60*1/h), np.repeat(b[1], 60*1/h), np.repeat(b[2], 60*1/h)]       # until day 60 constant 
+            b2M = -(R0[0]-0.3)*a/120*T_vec[int(60*1/h):int(180/h)] + b[0] - -(R0[0]-0.3)*a/120*60  # linear decrease
+            b2V = -(R0[1]-0.3)*a/120*T_vec[int(60*1/h):int(180/h)] + b[0] - -(R0[1]-0.3)*a/120*60
+            b2N = -(R0[2]-0.3)*a/120*T_vec[int(60*1/h):int(180/h)] + b[0] - -(R0[2]-0.3)*a/120*60
+            b3 = np.repeat(0.3*a, T*1/h-(180)*1/h)# until end constant
+            self.beta_array= np.array([np.hstack((b1[0], b2M, b3)),np.hstack((b1[1], b2V, b3)),np.hstack((b1[2], b2N, b3))])
+
+            #gamma
+            gamma_mexico = 180*self.g[0] / (np.sqrt(2.0 * np.pi) * 30) * np.exp(-np.power((T_vec - 90) / 30, 2.0) / 2)
+            gamma_van = 180*self.g[1] / (np.sqrt(2.0 * np.pi) * 30) * np.exp(-np.power((T_vec - 90) / 30, 2.0) / 2)
+            gamma_NY = 180*self.g[2] / (np.sqrt(2.0 * np.pi) * 30) * np.exp(-np.power((T_vec - 90) / 30, 2.0) / 2)
+            self.gamma_array = g= np.array([gamma_mexico,gamma_van,gamma_NY])
+        
+        elif False: # no vaccination, linear beta
+            self.T = T
+            self.alpha = 1/6
+            T_vec = np.arange(0,T,h)
+            #beta
+            b = [a*R0[0], a*R0[1], a*R0[2]]
+            b1 = [np.repeat(b[0], 60*1/h), np.repeat(b[1], 60*1/h), np.repeat(b[2], 60*1/h)]       # until day 60 constant 
+            b2M = -(R0[0]-0.3)*a/120*T_vec[int(60*1/h):int(180/h)] + b[0] - -(R0[0]-0.3)*a/120*60  # linear decrease
+            b2V = -(R0[1]-0.3)*a/120*T_vec[int(60*1/h):int(180/h)] + b[0] - -(R0[1]-0.3)*a/120*60
+            b2N = -(R0[2]-0.3)*a/120*T_vec[int(60*1/h):int(180/h)] + b[0] - -(R0[2]-0.3)*a/120*60
+            b3 = np.repeat(0.3*a, T*1/h-(180)*1/h)# until end constant
+            self.beta_array= np.array([np.hstack((b1[0], b2M, b3)),np.hstack((b1[1], b2V, b3)),np.hstack((b1[2], b2N, b3))])
+
+            #gamma
+            self.gamma_array =  np.array([np.repeat(0, T/h),np.repeat(0, T/h),np.repeat(0, T/h)])
+
+        # No vaccination 
         elif False: # adapted values for mexico, PEAK FOR FIRST WAVE 
             self.alpha = 1/6
             #calculation beta
             b = [a*R0[0], a*R0[1], a*R0[2]]
             #self.beta_array= np.array([np.repeat(b[0], T/h),np.repeat(b[1], T/h),np.repeat(b[2], T/h)])
             self.beta_array= np.array([np.repeat(b[0], T/h),np.repeat(b[1], T/h),np.repeat(b[2], T/h)])
-            self.gamma_array = g= np.array([np.repeat(0, T/h),np.repeat(0, T/h),np.repeat(0, T/h)])
+            self.gamma_array =  np.array([np.repeat(0, T/h),np.repeat(0, T/h),np.repeat(0, T/h)])
 
-        elif True: # Quarantine in Mexico included, PEAK FOR FIRST WAVE  ### Recomments: R0 = 2.5, a = 1/6, infected = 100
-            if h == 1: 
-                R0 = [1.7, 1.6, 1.74]
-                self.alpha = 1/6
-                #calculation beta
-                b = [a*R0[0], a*R0[1], a*R0[2]]
-                b1 = np.repeat(b[0], 37)
-                b15= np.repeat(1.0*a, 3)
-                b2 = np.repeat(0.5*a,11)
-                b3 = np.repeat(1.1*a, T-(40 + 11))
-                self.beta_array= np.array([np.hstack((b1, b15, b2,b3)),np.repeat(b[1], T/h),np.repeat(b[2], T/h)])
-                self.gamma_array = g= np.array([np.repeat(0, T/h),np.repeat(0, T/h),np.repeat(0, T/h)])
-            else:
-                print('Set h = 1')
 
-        else: # No vaccination
+        elif False: # Quarantine in Mexico included, PEAK FOR FIRST WAVE  ### Recomments: infected = 10, no vaccination
+            # TO
+            self.T = 75
+            R0 = [1.72, 1.6, 1.74]
+            self.alpha = 1/6
+            T_vec = np.arange(0,T,h)
+            #calculation beta
+            b = [a*R0[0], a*R0[1], a*R0[2]]
+            b1 = np.repeat(b[0], 37*1/h)       # until day 37 constant 
+            b15= -1.22*a/3*T_vec[int(37*1/h):int(40/h)] + 2.79 # until day 40 linear decrease
+            b2 = np.repeat(0.5*a,11*5)         # until day 51 constant
+            b25 = 0.4*a/5*T_vec[int(51*1/h):int(56/h)] - 0.596 # until day 56 linear increase
+            b3 = np.repeat(0.9*a, T*1/h-(40 + 11 + 5)*1/h) # until end constant
+            self.beta_array= np.array([np.hstack((b1, b15, b2, b25, b3)),np.repeat(b[1], T/h),np.repeat(b[2], T/h)])
+            self.gamma_array =  np.array([np.repeat(0, T/h),np.repeat(0, T/h),np.repeat(0, T/h)])
+
+
+        else: # No vaccination, constant beta
             self.alpha = a
+            self.g = [0,0,0]
+            b = [a*R0[0], a*R0[1], a*R0[2]]
             self.beta_array= np.array([np.repeat(b[0], T/h),np.repeat(b[1], T/h),np.repeat(b[2], T/h)])
-            self.gamma_array = g= np.array([np.repeat(0, T/h),np.repeat(0, T/h),np.repeat(0, T/h)])
+            self.gamma_array =  np.array([np.repeat(0, T/h),np.repeat(0, T/h),np.repeat(0, T/h)])
 
-        ## ToDo: TimeDependence over time
 
         #print values
         print('T = ', T, 'Population0 = ', pop_0, 'beta = ' ,self.beta_array[:,0], 'gamma  = ', self.gamma_array[:,0], 'alpha = ', self.alpha)
@@ -65,12 +124,12 @@ class Disease:
         self.beta = self.beta_array[:,0]
         self.gamma = self.gamma_array[:,0]
 
-        # Transport matrix: [default as in Jakobs example] - TODO
-        if False: # Traveling            
+        # Transport matrix: 
+        if True: # Traveling            
             
             #calculating probabilities
-            W_NM = 0.24*640E3*(pop_0[2,0]/308E6)/pop_0[2,0]
-            W_NV = 0.11*0.8*2999.330/pop_0[2,0]
+            W_NM = 0.14*6400000*0.06/pop_0[2,0]
+            W_NV = 0.11*0.8*2.89E6/pop_0[2,0]
             W_MV = 0.7*0.8*63.5E3/pop_0[0,0]
 
             W_MN = W_NM*pop_0[2,0]/pop_0[0,0]
@@ -78,16 +137,10 @@ class Disease:
             W_VM = W_MV*pop_0[0,0]/pop_0[1,0]
 
             self.W = np.array([[0, W_VM, W_NM],[W_MV ,0, W_NV],[W_MN , W_VN,0]])
+            print(self.W)
 
-        elif True: # NO travaling
+        else: #No travaling
             self.W = np.array([[0, 0, 0],[0,0, 0],[0 , 0,0]]) 
-
-        else: # Jakobs expample
-            self.W = np.array([[0.0, 5.27324458E-05 ,2.38237046E-04],
-                            [9.28720687E-05 ,0.0 ,2.97796307E-05],
-                            [8.17828665E-04, 5.80450430E-05 ,0.0]])
-
-        print("W = ", self.W)
 
         #Euler & RK parameters
         self.h = h # step size [default: 1 day]
@@ -219,8 +272,8 @@ class Disease:
             
             y0 = self.population_0 
             self.y_RK_SIR[0] = y0
-            
-            for i in range(1, (self.n)+1):
+            print(self.n)
+            for i in range(1, (self.n) +1):
                 self.beta = self.beta_array[:,i-1]
                 self.gamma = self.gamma_array[:,i-1]
                 self.y_RK_SIR[i] = self.RungeKuttaStep(self.x[i-1], self.y_RK_SIR[i-1], model)
@@ -303,23 +356,45 @@ class Disease:
 # ys is a list of y-type lists   
 #labels is the list of labels, one label per each line in the plot  
 # xlabel and ylabel refer to the whole plot     
-def plot(xs, ys, labels, colors, linestyles, title, xlabel, ylabel):
-    plt.style.use('rc.mplstyle')
-    fig, ax = plt.subplots(figsize = (3,4))
-    ax.set_title(title)
-    ax.set_xlabel(xlabel)
-    ax.set_ylabel(ylabel)
-    ax.grid(True)
+    def plot(self, xs, ys, labels, colors, linestyles, title, xlabel, ylabel):
+
+        plt.style.use('rc.mplstyle')
+
+        fig, ax = plt.subplots(figsize = (3,4))
+        ax.set_title(title)
+        ax.set_xlabel(xlabel)
+        ax.set_ylabel(ylabel)
+        ax.grid(True)
+
     
-    for line in range(len(xs)):
-        ax.plot(xs[line], ys[line], label = labels[line], 
-                color = colors[line], linestyle = linestyles[line])
-    ax.set_xlim(xs[0][0], xs[0][-1])
-    ax.legend()      
-      
-    fig.tight_layout()    
-    plt.savefig(save_title + '.pdf') 
-    plt.show()
+        for line in range(len(xs)):
+            ax.plot(xs[line], ys[line], label = labels[line], 
+                    color = colors[line], linestyle = linestyles[line])
+        ax.set_xlim(xs[0][0], xs[0][-1])
+
+        
+        #Mexico Quarantäne
+        ax.text(0.22,0.78, f' $\\beta_1  $ = {self.R0[0]*self.a:.3f}\n $\\beta_2  $ = {0.4*self.a:.3f}\n $\\beta_3  $ = {0.9*self.a:.3f}\n$\\gamma  $ = {self.g[0]*self.a:.3f}',fontsize=9, transform=ax.transAxes, verticalalignment='top', horizontalalignment='center', bbox=dict(facecolor='lightgrey', alpha=0.2, edgecolor='lightgrey'))
+        ax.vlines(x = 37, ymin = 0, ymax = 1, transform=ax.get_xaxis_transform(), colors= 'slategrey')
+        ax.vlines(x = 40, ymin = 0, ymax = 1, transform=ax.get_xaxis_transform(), colors= 'darkgrey')
+        ax.vlines(x = 51, ymin = 0, ymax = 1, transform=ax.get_xaxis_transform(), colors= 'darkgrey')
+        ax.vlines(x = 56, ymin = 0, ymax = 1, transform=ax.get_xaxis_transform(), colors= 'slategrey')
+        ax.text(0.22,0.45, f'$\\beta_1  $ ',fontsize=10, transform=ax.transAxes, verticalalignment='top', horizontalalignment='center')
+        ax.text(0.6,0.45, f'$\\beta_2  $ ',fontsize=10, transform=ax.transAxes, verticalalignment='top', horizontalalignment='center')
+        ax.text(0.85,0.45, f'$\\beta_3  $ ',fontsize=10, transform=ax.transAxes, verticalalignment='top', horizontalalignment='center')
+        
+
+        # const beta, gamma
+        #ax.text(0.22,0.55, f'$\\beta$ = {self.R0[0]*self.a:.3f}\n $\\gamma  $ = {self.g[0]*self.a:.3f}\n',fontsize=12, transform=ax.transAxes, verticalalignment='top', horizontalalignment='center')
+        
+        # Gauss
+        #ax.text(0.22,0.3, f'$\\beta$ = {self.R0[0]*self.a:.3f}\n \t\t\t$\\gamma = 0.003 \cdot 180 \cdot N(90, 30)$ \n',fontsize=12, transform=ax.transAxes, verticalalignment='top', horizontalalignment='center')
+
+        ax.legend()      
+        
+        fig.tight_layout()    
+        plt.savefig(save_title + '.pdf') 
+        plt.show()
       
 
 if __name__ == '__main__':
@@ -351,7 +426,7 @@ if __name__ == '__main__':
     theo_ls =  '-.'
     
     #la for labels, order is S, I, R
-    RK_la = ("RK4 $S(t)$", "RK4 $I(t)$", "RK4 $R(t)$")
+    RK_la = ("$S(t)$", "$I(t)$", "$R(t)$")
     pEu_la = ("Eu $S(t)$", "Eu $I(t)$", "Eu $R(t)$")
     theo_la = ("exact $S(t)$", "exact $I(t)$", "exact $R(t)$")
     
@@ -392,18 +467,19 @@ if __name__ == '__main__':
         
         test1.RungeKuttaLoop(model)
         
-        title = "S(t), I(t) and R(t) on SIR: RK vs days"
-        save_title = "SIR"
+        title = ""
+        print(test1.beta)
+        save_title = "SIR-Mexico-quaran" + str(test1.beta[0]) +"_" +   str(test1.gamma[0])  + "_" + str(test1.alpha) + "_" + str(test1.h) +"_" +  str(test1.T) 
         
-        y_axis = [test1.y_RK_SIR[:,0,1]] #, test1.y_RK_SIR[:,0,1], test1.y_RK_SIR[:,0,2]]
+        y_axis = [test1.y_RK_SIR[:,0,2] , test1.y_RK_SIR[:,0,1]]#, test1.y_RK_SIR[:,0,2]]
         
-        labels = [RK_la[0], RK_la[1], RK_la[2]]
+        labels = [RK_la[2], RK_la[1], RK_la[2]]
         colors = [S_co, I_co, R_co]
         linestyles = [RK_ls, RK_ls, RK_ls]
         
         x_axis = [test1.x for i in range(len(y_axis))]
         
-        plot(x_axis, y_axis, labels, colors, linestyles, title, "Days", "Population")
+        test1.plot(x_axis, y_axis, labels, colors, linestyles, title, "Days", "Population")
 
     ###############################################################################
 
@@ -456,16 +532,47 @@ if True:
         col.grid(True)
 
         #Plots
-        #col.plot(test1.x, test1.y_RK_SIT[:,i,0], label = f'$S(t)$', color = S_co) 
+        col.plot(test1.x, test1.y_RK_SIT[:,i,0], label = f'$S(t)$', color = S_co) 
         col.plot(test1.x, test1.y_RK_SIT[:,i,1], label = f'$I(t)$', color = I_co)
-        #col.plot(test1.x, test1.y_RK_SIT[:,i,2], label = f'$R(t)$', color = R_co)
-        #col.set_xticks(np.arange(test1.x[0], test1.x[-1], len(test1.x)/4))
+        col.plot(test1.x, test1.y_RK_SIT[:,i,2], label = f'$R(t)$', color = R_co)
+        col.ticklabel_format(axis='y', style='sci', scilimits=(0,0))
+        col.set_xticks([0,100,200,300])
+    
         col.set_xlim(test1.x[0], test1.x[-1])
+
+        #Plot settings for different beta and gamma
+
+        # const beta, gamma
+        #col.text(0.33,0.9, f'$\\beta$ = {test1.R0[i]*test1.a:.3f}\n $\\gamma  $ = {test1.g[i]*test1.a:.3f}',fontsize=9, transform=col.transAxes, verticalalignment='top', horizontalalignment='center', bbox=dict(facecolor='lightgrey', alpha=0.6, edgecolor='lightgrey'))
+        
+        # const beta, Gauss - gamma
+        col.text(0.7,0.25, f'$\\beta$ = {test1.R0[i]*test1.a:.3f} \n $ \\gamma = $ Gauss ',fontsize=9, transform=col.transAxes, verticalalignment='top', horizontalalignment='center', bbox=dict(facecolor='lightgrey', alpha=0.2, edgecolor='lightgrey'))
+        
+        '''
+        # lin beta, zero gamma
+        col.text(0.35,0.65, f'$\\beta_1$ = {test1.R0[i]*test1.a:.3f} \n $\\beta_2$ = {0.3*test1.a:.3f}\n $ \\gamma = 0.000$',fontsize=9, transform=col.transAxes, verticalalignment='top', horizontalalignment='center', bbox=dict(facecolor='lightgrey', alpha=0.6, edgecolor='lightgrey'))
+        col.vlines(x = 60, ymin = 0, ymax = 1, transform=col.get_xaxis_transform(), colors= 'slategrey')
+        col.vlines(x = 180, ymin = 0, ymax = 1, transform=col.get_xaxis_transform() , colors= 'darkgrey')
+        col.text(0.10,0.28, f'$\\beta_1$ ',fontsize=9, transform=col.transAxes, verticalalignment='top', horizontalalignment='center')
+        col.text(0.45,0.28, f'$\\beta_l$ ',fontsize=9, transform=col.transAxes, verticalalignment='top', horizontalalignment='center')
+        col.text(0.80,0.28, f'$\\beta_2$ ',fontsize=9, transform=col.transAxes, verticalalignment='top', horizontalalignment='center')
+        '''
+
+        '''
+        # lin beta, gauss gamma
+        #col.text(0.680,0.95, f'$\\beta_1$ = {test1.R0[i]*test1.a:.3f} \n $\\beta_2$ = {0.3*test1.a:.3f}\n $ \\gamma = $ Gauss ',fontsize=9, transform=col.transAxes, verticalalignment='top', horizontalalignment='center', bbox=dict(facecolor='lightgrey', alpha=0.2, edgecolor='lightgrey'))
+        col.vlines(x = 60, ymin = 0, ymax = 1, transform=col.get_xaxis_transform() , colors= 'slategrey')
+        col.vlines(x = 180, ymin = 0, ymax = 1, transform=col.get_xaxis_transform() , colors= 'darkgrey')
+        col.text(0.10,0.23, f'$\\beta_1$ ',fontsize=9, transform=col.transAxes, verticalalignment='top', horizontalalignment='center')
+        col.text(0.80,0.23, f'$\\beta_2$ ',fontsize=9, transform=col.transAxes, verticalalignment='top', horizontalalignment='center')
+        col.text(0.360,0.23, f'$\\beta_l$ ',fontsize=9, transform=col.transAxes, verticalalignment='top', horizontalalignment='center')
+        '''
+
         i+= 1
 
     handles, labels = ax[0].get_legend_handles_labels()
     fig.legend(handles, labels, bbox_to_anchor=(0.5, 0.995), loc='upper center', ncols=3)
   
-    fig.subplots_adjust(wspace=0.28, left=0.1, right=0.97, top=0.82, bottom=0.25)
-    plt.savefig('SRT_3Plots.pdf') 
+    fig.subplots_adjust(wspace=0.36, left=0.1, right=0.97, top=0.82, bottom=0.25)
+    plt.savefig('SRT_const_beta_gauss_vacc_300.pdf') 
     plt.show()
